e950a8b6f4e9c0f463b95178bc488b3016939484
Aug5th <namhnt.0508@gmail.com>
2025-12-28 13:48:22 +0700

    Add Item Rarity

--- Assets/Scripts/Item/ItemDefinition.cs
+++ Assets/Scripts/Item/ItemDefinition.cs
@@ -4,7 +4,8 @@ public class ItemDefinition : ScriptableObject
 {
     [Header("Basic Info")]
     public string id;
-    public ItemType itemType;      
+    public ItemType itemType;
+    public ItemRarity itemRarity;      
     public string itemName; 
     public Sprite icon; 
     
--- Assets/Scripts/Item/ItemEnums.cs
+++ Assets/Scripts/Item/ItemEnums.cs
@@ -1,4 +1,13 @@
 
+public enum ItemRarity
+{
+    Common = 0,
+    Uncommon = 1,
+    Rare = 2,
+    Epic = 3,
+    Legendary = 4
+}
+
 public enum ItemType
 {
     Consumable = 0,
